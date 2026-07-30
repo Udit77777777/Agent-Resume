f20dfd4c761cf6fc1e7366d2584111f1b9d0d647
Udit Giri <gkguditgiri@gmail.com>
2026-07-30 11:05:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -187,4 +187,21 @@ Requirements:
    #code = get_jobs(agent)
    #DISPLAY.HTML(code)
 
+if st.button("Generate Resume"):
+    with st.spinner("Agent Running"):
+        code = main_agent(agent, user_info)
+        st.html(
+            code,
+            width="stretch",
+            unsafe_allow_javascript=True
+        )
+
+        st.divider()  # to give horizontal div
+
+        jobe_code = get_jobs(agent, location, profile)
+        st.html(
+            jobe_code,
+            width="stretch",
+            unsafe_allow_javascript=True
+        )
 
